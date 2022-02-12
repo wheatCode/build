08ae519322b4c8320446669b7d7efd5695bac030
Ming <ming@Mingde-MacBook-Pro.local>
2022-02-13 00:33:13 +0800

    update image url

--- modules/repair/js_action/repair_show_repair_page.js
+++ modules/repair/js_action/repair_show_repair_page.js
@@ -241,7 +241,7 @@ class repair_show_repair_page extends ActionHandler {
                             else if (y) { txtId = 2; }
                             else { txtId = 2; }
                             //$("#timearea").append('<div class="row" id="div' + txtId + '"><div class="col-1"></div><div class="col-3 p-0"><input type="date" style="min-width:100%"class="form-control" name="test[]" id="date' + txtId + '" placeholder="Enter month" /></div><div class="col-3 p-0"><input type="text" name="test[]" style="min-width:100%"class="form-control timepicker" id="input_starttime' + txtId + '-1" placeholder="起始時間"/></div><div class="col-3 p-0"><input type="text" style="min-width:100%"class="form-control timepicker" name="test[]"id="input_starttime' + txtId + '-2" placeholder="結束時間"/></div><div class="col-1 p-0 w-32"><button type="button" id="btnd' + txtId + '" class="btn btn-primary m-0 p-0"  style="height:38px;width:38px;" value="del">X</button></div></br>');
-                            $("#timearea").append(`<div class="row mt-0 pt-0 pl-1 pb-3" id="div` + txtId + `" >
+                            $("#timearea").append(`<div class="row mt-0 pt-0 pb-3" id="div` + txtId + `" >
                             <div class="col-4">
                             <input type="date" style="min-width:100%"class="form-control" id="date1" placeholder="Enter month">
                         </div>
