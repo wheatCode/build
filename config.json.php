a8a681aa78652f7d45a7d1ab37f75e543a4e8b57
Ming <ming@Mingde-MacBook-Pro.local>
2022-02-12 14:59:55 +0800

    update m

--- config.json.php
+++ config.json.php
@@ -3,8 +3,8 @@
     "db": {
         "db_host":"wheat0710.tw",
         "db_name":"build",
-        "db_user":"wheat0710",
-        "db_password":"wheat0710"
+        "db_user":"wheat07",
+        "db_password":"wheat07"
     }
 }
 
