a4a00925ca5aac24a0f61278dd2f0cfb2efc7170
Ming <ming@Mingde-MacBook-Pro.local>
2022-02-12 22:36:11 +0800

    update image url

--- modules/news/js_action/news_do_select_action.js
+++ modules/news/js_action/news_do_select_action.js
@@ -18,7 +18,7 @@ class news_do_select_action extends ActionHandler {
         str += `<div id="area">`;
         for (var i in info) {
 
-            str += `<div class="row"><div class="col-12"><a  onclick="(new news_show_news_page('news', 'show_news_page', 'body', '${info[i]["id"]}')).run();"><div class="row mt-2"><div class="col px-0"><div class="row border boxShadow mx-1 mt-0"><div class="col-5 px-0">`;
+            str += `<div class="row mt-0"><div class="col-12"><a  onclick="(new news_show_news_page('news', 'show_news_page', 'body', '${info[i]["id"]}')).run();"><div class="row mt-2"><div class="col px-0"><div class="row border boxShadow mx-1 mt-0"><div class="col-5 px-0">`;
             str += '<img src="' + obj['news_img'][i] + '" alt="" srcset="" width="85px" height="80px"></div><div class="col-7 pl-0 pr-1 my-0"><h5 class="font-weight-bold my-1">';
             str += info[i]['topic'] + '</h5></div></div></div></div></a></div></div>';
 
