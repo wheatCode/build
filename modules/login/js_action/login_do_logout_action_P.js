e52521165fc985e4af57b64e241539b96dd8bdaa
Ming <ming@Mingde-MacBook-Pro.local>
2022-02-14 00:28:56 +0800

    update image url

--- modules/login/js_action/login_do_logout_action_P.js
+++ modules/login/js_action/login_do_logout_action_P.js
@@ -12,13 +12,13 @@ class login_do_logout_action_P extends ActionHandler {
             var json_str = xhttp.responseText;
             var obj = JSON.parse(json_str);
             if (obj['status_code'] === 0) { //0為登入成功 -100失敗(帳密錯誤)
-                this.loadModuleScript('login', 'show_login_page_P');
                 (new login_show_login_page_P('login', 'show_login_page_P', 'body')).run();
+                this.loadModuleScript('login', 'show_login_page_P');
+
               
             }
             else {
-                this.loadModuleScript('login', 'show_login_page_P');
-                (new login_show_login_page_P('login', 'show_login_page_P', 'body')).run();
+                console.log("shit happend");
                
             }
         }
