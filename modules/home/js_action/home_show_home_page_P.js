da492c66ca393dfb5019ef07750418172cb0e103
Ming <ming@Mingde-MacBook-Pro.local>
2022-02-14 01:30:13 +0800

    update image url

--- modules/home/js_action/home_show_home_page_P.js
+++ modules/home/js_action/home_show_home_page_P.js
@@ -18,7 +18,7 @@ class home_show_home_page_P extends ActionHandler {
             this.loadModuleScript("news", "show_select_page_P");
             this.loadModuleScript("building","show_management_page");
             this.loadModuleScript("login", "do_logout_action_P");
-            this.loadModuleScript("login", "show_login_page_P");
+            this.loadModuleScript("login", "show_login_page_p");
             if (obj['status_code'] === 1) {
                 var str = `
                 <header>
@@ -92,8 +92,8 @@ class home_show_home_page_P extends ActionHandler {
             }
             else if (obj['status_code'] == 2) {
                 // alert("後臺管理");
-                (new login_show_login_page_P("login", "show_login_page_P", "body")).run();
-                var script = this.loadModuleScript("login", "show_login_page_P");
+                (new login_show_login_page_p("login", "show_login_page_p", "body")).run();
+                var script = this.loadModuleScript("login", "show_login_page_p");
 
                 
             }
