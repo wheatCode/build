f1bf739f5e4093c8ed3aba9b0bb38e70925fe3d6
Ming <ming@Mingde-MacBook-Pro.local>
2022-02-14 00:16:14 +0800

    update image url

---
+++ modules/login/js_action/login_show_login_page_P.js
@@ -0,0 +1,49 @@
+class login_show_login_page_P extends ActionHandler {
+    constructor(module, action, position_id) {
+        super(module, action);
+        this.position_id = position_id;
+    }
+    prepareArgs() {
+        this.php = false;
+    }
+    showResult() {
+        var str = `
+          <div class="container ">
+        <div class="col-lg-6 col-md-6">
+            <!-- Start your project here-->
+            <section class="form-dark">
+                <!-- Material form login -->
+                <div class="container p-3 mt-5">
+                    <form>
+                        <div class="text-center mb-3 col-md-12">
+                            <a href="#"><img src="/logo/logo.png" class="img-fluid flex-center"></a>
+                        </div>
+                        <!-- Material input email -->
+                        <div class="md-form text-gray">
+                            <i class="fa fa-envelope prefix gray-text"></i>
+                            <input type="text" id="account" class="form-control gray-text">
+                            <label for="account">電子郵件</label>
+                        </div>
+                        <div class="md-form pb-3 ">
+                            <i class="fa fa-lock prefix gray-text"></i>
+                            <input type="password" id="password" class="form-control gray-text">
+                            <label for="password">密碼</label>
+                            <div id="login_err_msg"></div>
+                        </div>
+
+                        <div class="text-center mb-3 col-md-12">
+                            <button type="button" class="btn btn-success btn-block btn-rounded z-depth-1" onclick="(new login_do_login_action('login','do_login_action','body')).run()">登入</button>
+                        </div>
+                    </form>
+                </div>
+                <!-- Material form login -->
+            </section>
+            <!--Section: Live preview-->
+        </div>
+    </div>	`;
+        this.loadScript("include/lib/CryptoJSv3.1.2/rollups/aes.js", "CryptoJS_AES");
+
+        document.getElementById(this.position_id).innerHTML = str;
+        this.loadModuleScript('login', 'do_login_action_P');
+    }
+}
