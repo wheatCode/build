700e812c05cc196f5ec55ca23b8d0b644443703e
Ming <ming@Mingde-MacBook-Pro.local>
2022-02-14 01:28:33 +0800

    update image url

--- modules/home/js_action/home_show_home_page_P.js
+++ modules/home/js_action/home_show_home_page_P.js
@@ -92,7 +92,7 @@ class home_show_home_page_P extends ActionHandler {
             }
             else if (obj['status_code'] == 2) {
                 // alert("後臺管理");
-                (new login_show_login_page_P("login", "show_login_page_P", "body")).run();
+                (new login_show_login_page_p("login", "show_login_page_p", "body")).run();
                 var script = this.loadModuleScript("login", "show_login_page_P");
 
                 
