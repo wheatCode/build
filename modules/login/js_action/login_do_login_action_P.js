485fc1110544e9dccb5ec1d5b1f0bdf7d5342689
Ming <ming@Mingde-MacBook-Pro.local>
2022-02-14 01:51:47 +0800

    update image url

--- modules/login/js_action/login_do_login_action_P.js
+++ modules/login/js_action/login_do_login_action_P.js
@@ -18,7 +18,7 @@ class login_do_login_action_P extends ActionHandler {
             var json_str = xhttp.responseText;
             var obj = JSON.parse(json_str);
 
-            console.log(obj['status_code']);
+            console.log(obj);
             if (obj['status_code'] === 0) { //0為登入成功 -100失敗(帳密錯誤)
                 //var str = `<button type="button" class="btn btn-success btn-block btn-rounded z-depth-1" onclick="(new home_show_home_page('home', 'show_home_page', 'body')).run()">登入</button>`;
                 //document.getElementById(this.position_id).innerHTML = str;
